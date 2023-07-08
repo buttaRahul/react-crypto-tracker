84d16e0cd79f459217092d7a54a93d0f5ee9ddb3
Rahul Aditya <butta.rahul.aditya@gmail.com>
2023-07-08 21:35:48 +0530

    Implemented Header with states, useContext and useEffect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
 
 const Header = () => {
-  return (
-    <div>
-        <p>Header</p>
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Header
+  const { currency, setCurrency } = CryptoState();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      mode: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => {
+                navigate("/");
+              }}
+              style={{
+                flex: 1,
+                color: "gold",
+                fontFamily: "Mukta",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+              variant="h6"
+            >
+              Crypto Tracker
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import CryptoContext from './CryptoContext.jsx'
+
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
- 
+    <CryptoContext>
+        <App />
+    </CryptoContext>
 )
