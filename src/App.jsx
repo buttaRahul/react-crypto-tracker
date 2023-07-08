45d09b17186a25244aaf87a705647e5d1ee6bc10
Rahul Aditya <butta.rahul.aditya@gmail.com>
2023-07-08 20:20:52 +0530

    Added Routers and Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import CoinPage from "./pages/CoinPage";
+import './App.css'
 
 function App() {
 
-
   return (
-    <>
-    <p> TOMATO </p>
-     
-    </>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+        <Route path="/" element= {<HomePage/>}/>
+        <Route path="/coins/:id" element = {<CoinPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
